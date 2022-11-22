94e4b06eb8ec2a8d563aa39cae9ca9e7fe256882
Ivan Sambol <117811964+Sambarone@users.noreply.github.com>
2022-11-22 20:51:59 +0100

    Unos ostalih podataka u bazu i označavanje obaveznih polja
    Unos polaznika, predavača, smjerova i nove grupe. Označavanje obaveznih polja prilikom novog unosa podataka.
    Postavljanje načina kodiranja utf8.

--- skriptapp26.sql
+++ skriptapp26.sql
@@ -2,54 +2,55 @@
 # Izvođenje naredbi na serveru
 # Otvoriti CMD
 # Zaljepiti sljedeću naredbu bez prvog hash znaka
-# c:\xampp\mysql\bin\mysql -uroot < C:\Users\Samba\Documents\GitHub\vjezbanje\Vjezbanje\skriptapp26.sql
+#naredba ispod označava postavke kodriranja
+# c:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\Samba\Documents\GitHub\vjezbanje\Vjezbanje\skriptapp26.sql
 
 
 
 drop database if exists edunovapp26;
-create database edunovapp26;
+create database edunovapp26 default charset utf8;
 use edunovapp26;
 
 create table smjer(
     sifra int not null primary key auto_increment,
-    naziv varchar(50),
+    naziv varchar(50) not null,
     cijena decimal(18,2),
     upisnina decimal(18,2),
-    trajanje int,
-    certificiran boolean
+    trajanje int not null,
+    certificiran boolean not null
 );
 
 create table grupa(
     sifra int not null primary key auto_increment,
-    naziv varchar(50),
-    maksimalnopolaznika int,
+    naziv varchar(50) not null,
+    maksimalnopolaznika int not null,
     datumpocetka datetime,
-    smjer int,
+    smjer int not null,
     predavac int
 );
 
 create table osoba(
     sifra int not null primary key auto_increment,
-    ime varchar(50),
-    prezime varchar(50),
-    email varchar(50),
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    email varchar(50) not null,
     oib char(11)
 );
 
 create table polaznik(
     sifra int not null primary key auto_increment,
     broj_ugovora varchar(20),
-    osoba int
+    osoba int not null
 );
 
 create table predavac(
     sifra int not null primary key auto_increment,
     iban varchar(50),
-    osoba int
+    osoba int not null
 );
 
 create table clan(
-    grupa int,
+    grupa int not null,
     polaznik int
 );
 
@@ -66,11 +67,33 @@ alter table predavac add foreign key (osoba) references osoba(sifra);
 
 #-unos podataka u tablicu smjer
 insert into smjer (sifra,naziv,cijena,upisnina,trajanje,certificiran)
-values (null,'PHP26',6950,500,130,false);
+values (null,'PHP programiranje',6950,500,130,false),
+(null,'Java programiranje',6950,500,140,true), (null,'Serviser',7000.50,500,180,true);
+
+# unos podataka u tablicu grupa 
+#(obavezan unos za atribute koji imaju vrijednost not null)
+# datum se piše u jednostrukim navodnicima i u sljedećem formatu 2022-11-07 20:00:00
+insert into grupa (sifra, naziv, maksimalnopolaznika,datumpocetka,smjer)
+values (null,'PHP26',20,'2022-11-07 19:00:00',1), (null,'JS26',20,'2022-11-07 17:00:00',2);
+
+# unos podatka u tablicu osoba
+insert into osoba (sifra,ime,prezime,email)
+values (null,'Marko','Marković','marko.markovic@gmail.com'),
+(null,'Ivo','Ivic','ivo.ivic@gmail.com'),
+(null,'Josip','Josipović','josip.josipovic@gmail.com'),
+(null,'Marija','Marić','marija.maric@gmail.com'),
+(null,'Ana','Anić','ana.anic@gmail.com'),
+(null,'Ena','Dalić','ena-dalic@gmail.com'),
+(null,'Tomislav','Jakopec','t.jakopec@gmail.com');
+
+#unos podataka u tablicu polaznik
+insert into polaznik (osoba)
+values (1),(2),(3),(4),(5),(6);
 
 
-
-
+#unos podataka u tablicu polaznik
+insert into predavac(osoba)
+values (7);
 
 
 
