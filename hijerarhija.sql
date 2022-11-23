0cc5c936fc4e9e8f3017dc75c0c7f67e95bc8c99
Ivan Sambol <117811964+Sambarone@users.noreply.github.com>
2022-11-23 17:32:38 +0100

    Unos zaposlenika, potavljanje nadređenog
    Unos 2 zaposlenika Ivana i Željka, postavljanje Ivana za nadređenog Željku i postavljanje naredbe za pokretanje u CMDu na vrhu baze podataka.

--- hijerarhija.sql
+++ hijerarhija.sql
@@ -1,3 +1,6 @@
+
+#c:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\Samba\Documents\GitHub\vjezbanje\Vjezbanje\hijerarhija.sql
+
 drop database if exists hijerarhija;
 create database hijerarhija;
 use hijerarhija;
@@ -12,3 +15,10 @@ create table zaposlenik(
 
 alter table zaposlenik add foreign key (nadredjeni) references zaposlenik (sifra);
 
+#unos podataka u tablicu zaposlenik bez nadređenog jer provo moramo unjeti zaposlenike kako bi mogli proglastiti tko je nadređeni kome
+insert into zaposlenik (sifra,ime,prezime,placa)
+values(null,'Ivan','Sambol',6000), (null,'Zeljko','Skarica',5000);
+
+#postavljanje nadređenog
+update zaposlenik set nadredjeni =1
+where sifra=2;
