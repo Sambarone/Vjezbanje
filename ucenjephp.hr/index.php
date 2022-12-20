b223d8b363541bbb5ee35700b3ca6610b515f582
Ivan Sambol <117811964+Sambarone@users.noreply.github.com>
2022-12-20 17:32:13 +0100

    Izrada PHP stranica

---
+++ ucenjephp.hr/index.php
@@ -0,0 +1,24 @@
+
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <?php      include_once 'head.php';       ?>
+  </head>
+<body>
+
+    <div class="grid-container">
+
+    <?php   include_once 'izbornik.php';  ?>
+      <div class="grid-x grid-padding-x">
+        <div class="large-12 cell">
+          <h1>Welcome to Foundation</h1>
+        </div>
+      </div>
+
+      
+    </div>
+
+
+    <?php     include_once 'skripte.php';     ?>
+  </body>
+</html>
