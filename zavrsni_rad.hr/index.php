d81ee96fe71da0ea7a16ee796a6ae7df87344980
Ivan Sambol <117811964+Sambarone@users.noreply.github.com>
2022-12-27 19:46:56 +0100

    Zavrsni rad - zaglavlje, podnozje

---
+++ zavrsni_rad.hr/index.php
@@ -0,0 +1,16 @@
+
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <?php include_once 'zaglavlje.php'?>
+  </head>
+<body>
+
+    <div class="grid-container">
+      <div class='gornji'>
+        <h1>Karte.hr</h1>
+      </div>
+    </div>
+    <?php include_once 'podnozje.php' ?>
+  </body>
+</html>
