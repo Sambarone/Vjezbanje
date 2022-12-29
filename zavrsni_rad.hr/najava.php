09eb2c4b7a629f82f340238ca218762661137bf8
Ivan Sambol <117811964+Sambarone@users.noreply.github.com>
2022-12-29 16:47:59 +0100

    Uređivanje stranice

--- zavrsni_rad.hr/najava.php
+++ zavrsni_rad.hr/najava.php
@@ -1,5 +1,5 @@
-
-<div class="title-bar">
+<div class="nad">
+    <div class="title-bar">
   <div class="title-bar-left">
     
     <span class="title-bar-title">Najavni događaji</span>
@@ -7,7 +7,8 @@
   <div class="title-bar-right">
     
   </div>
-</div>
+    </div>
+
 
 <div class="media-object">
   <div class="media-object-section" id="najava">
@@ -16,10 +17,16 @@
   </div>
   <div class="media-object-section">
     <h4>Tamburaška rapsodija</h4>
-    <p>I'm going to improvise. Listen, there's something you should know about me... about inception. An idea is like a virus, resilient, highly contagious. The smallest seed of an idea can grow. It can grow to define or destroy you.</p>
+    <ul>
+    <li>Koncetrna dvorana Vatroslava Lisinskog, Zagreb</li>
+    <li>20:00 sati</li>
     <a class="button float-right" id="ulaznica" >Kupi ulaznice</a>
+    </ul>
+       
   </div>
 </div>
+
+<hr>
 
 <div class="media-object">
   <div class="media-object-section" id="najava">
@@ -28,11 +35,14 @@
   </div>
   <div class="media-object-section">
     <h4>Mačak u čizmama</h4>
-    <p>I'm going to improvise. Listen, there's something you should know about me... about inception. An idea is like a virus, resilient, highly contagious. The smallest seed of an idea can grow. It can grow to define or destroy you.</p>
+    <ul>
+    <li>Dječje kazalište Mala scena, Osijek</li>
+    <li>10:00 sati</li>
+    </ul>
     <a class="button float-right" id="ulaznica" >Kupi ulaznice</a>
   </div>
 </div>
-
+<hr>
 <div class="media-object">
   <div class="media-object-section" id="najava">
     <h2>11.01.</h2>
@@ -40,11 +50,14 @@
   </div>
   <div class="media-object-section">
     <h4>Božić u Ciboni</h4>
-    <p>I'm going to improvise. Listen, there's something you should know about me... about inception. An idea is like a virus, resilient, highly contagious. The smallest seed of an idea can grow. It can grow to define or destroy you.</p>
+    <ul>
+    <li>Sportska dvorana Dražen Petrović, Zagreb</li>
+    <li>18:00 sati</li>
+    </ul>
     <a class="button float-right" id="ulaznica" >Kupi ulaznice</a>
   </div>
 </div>
-
+<hr>
 <div class="media-object">
   <div class="media-object-section" id="najava">
     <h2>11.01.</h2>
@@ -52,11 +65,14 @@
   </div>
   <div class="media-object-section">
     <h4>Hrvatska - Tunis</h4>
-    <p>I'm going to improvise. Listen, there's something you should know about me... about inception. An idea is like a virus, resilient, highly contagious. The smallest seed of an idea can grow. It can grow to define or destroy you.</p>
+    <ul>
+    <li>Spaladium Arena, Split</li>
+    <li>20:00 sati</li>
+    </ul>
     <a class="button float-right" id="ulaznica" >Kupi ulaznice</a>
   </div>
 </div>
-
+<hr>
 <div class="media-object">
   <div class="media-object-section" id="najava">
     <h2>12.01.</h2>
@@ -64,7 +80,14 @@
   </div>
   <div class="media-object-section">
     <h4>Progledaj srcem</h4>
-    <p>I'm going to improvise. Listen, there's something you should know about me... about inception. An idea is like a virus, resilient, highly contagious. The smallest seed of an idea can grow. It can grow to define or destroy you.</p>
+    <ul>
+    <li>Sportska dvorana Doma sportova, Zagreb</li>
+    <li>20:00 sati</li>
+    </ul>
     <a class="button float-right" id="ulaznica" >Kupi ulaznice</a>
+    <br>
   </div>
+  
+</div>
+
 </div>
