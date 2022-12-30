1393ec7e32f8b1f79f6a98b1f2232a278b735671
Ivan Sambol <117811964+Sambarone@users.noreply.github.com>
2022-12-30 16:02:23 +0100

    Dovršetak registracije

--- zavrsni_rad.hr/registracija.php
+++ zavrsni_rad.hr/registracija.php
@@ -36,7 +36,7 @@
   </div>
 </form>
 
-<form>
+<form data-abide novalidate>
   <div class="grid-container">
     <div class="grid-x grid-padding-x">
       <div class="medium-6 cell">
@@ -57,7 +57,7 @@
     <div class="grid-x grid-padding-x">
       <div class="medium-6 cell">
         <label>Mjesto
-          <input type="text" placeholder="...">
+          <input type="text">
         </label>
       </div>
       <div class="medium-6 cell">
@@ -73,7 +73,7 @@
     <div class="grid-x grid-padding-x">
       <div class="medium-6 cell">
         <label>Država
-          <input type="text" placeholder="...">
+          <input type="text">
         </label>
       </div>
       <div class="medium-6 cell">
@@ -94,10 +94,45 @@
       </div>
       <div class="medium-6 cell">
         <label>lozinka
-          <input type="text" placeholder="...">
+          <input type="password">
         </label>
       </div>
     </div>
+  </div>
+
+  <div class="grid-container">
+    <div class="grid-x grid-padding-x">
+      <div class="medium-6 cell">
+        <label>Datum rođenja
+        <input type="date" name="time" id="time" />
+        </label>
+      </div>
+      <div class="medium-6 cell">
+        <label>Spol
+        <fieldset class="cell medium-6">
+      <input type="radio" name="pokemon" value="Red" id="pokemonRed"><label for="pokemonRed">Muško</label>
+      <input type="radio" name="pokemon" value="Blue" id="pokemonBlue" required><label for="pokemonBlue">Žensko</label>
+      </fieldset>
+        </label>
+      </div>
+    </div>
+
+    <fieldset class="cell medium-6">
+      <input id="checkbox1" type="checkbox"><label for="checkbox1">Potvrđujem da imam 16 godina starosti i više te da se slažem s Uvjeti korištenja i Izjavom o zaštiti osobnih podataka 
+        Karte.hr nije organizator ponuđenih događaja. Događaj vodi organizator, koji također izdaje ulaznice. 
+        Karte.hr djeluje samo kao distributer za prodaju ulaznica u ime organizatora i stoga ne snosi nikakvu odgovornost u vezi 
+        organizacije događaja i / ili njegovog održavanja.</label>
+       </fieldset>
+    
+    <div class="grid-x grid-margin-x">
+    <fieldset class="cell medium-6">
+      <button class="button" type="submit" value="Submit">Potvrdi</button>
+    </fieldset>
+    <fieldset class="cell medium-6">
+      <button class="button" type="reset" value="Reset">Poništi</button>
+    </fieldset>
+  </div>
+
   </div>
 </form>
 
