7c5f9a7b559ccef47650e01fb501a5916c07f9cd
Ivan Sambol <117811964+Sambarone@users.noreply.github.com>
2023-01-05 18:36:23 +0100

    Novi bootstrap
    Uspostavlanje css-a, js-a, formiranje stranice

--- boot.hr/head.php
+++ boot.hr/head.php
@@ -1,8 +1,12 @@
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Početna strana Boota</title>
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link href="css/headers.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/app.css">
-    
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.104.2">
+    <title> Bootstrap v5.2</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/cheatsheet/">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/cheatsheet.css" rel="stylesheet">
+    <link href="css/app.css" rel="stylesheet">
+
--- boot.hr/skripte.php
+++ boot.hr/skripte.php
@@ -1,5 +1,2 @@
-    <script src="js/bootstrap.bundle.js"></script>
-    <script src="js/bootstrap.esm.js"></script>
-    <script src="js/bootstrap.js"></script>
-    <script src="js/app.js"></script>
-    <script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/cheatsheet.js"></script>
