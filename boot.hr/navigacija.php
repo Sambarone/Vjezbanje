67b3f9fdbca9b481fdc196b2b343b65c961ba91e
Ivan Sambol <117811964+Sambarone@users.noreply.github.com>
2023-01-05 23:02:36 +0100

    Dodavanje slide show-a

---
+++ boot.hr/navigacija.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary mt-5">
+      <div class="container-fluid">
+
+          <div class="btn-group">
+          <button type="button" class="btn btn-primary">Događaji</button>
+          <button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+            <span class="visually-hidden">Toggle Dropdown</span>
+          </button>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Kultura</a></li>
+            <li><a class="dropdown-item" href="#">Koncerti</a></li>
+            <li><a class="dropdown-item" href="#">Sport</a></li>
+          </ul>
+        </div><!-- /btn-group -->
+
+        <div class="btn-group">
+          <button type="button" class="btn btn-primary">Lokacije</button>
+          <button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+            <span class="visually-hidden">Toggle Dropdown</span>
+          </button>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Zagreb</a></li>
+            <li><a class="dropdown-item" href="#">Osijek</a></li>
+            <li><a class="dropdown-item" href="#">Split</a></li>
+            <li><a class="dropdown-item" href="#">Rijeka</a></li>
+          </ul>
+        </div><!-- /btn-group -->
+      </div>
+ </nav>
