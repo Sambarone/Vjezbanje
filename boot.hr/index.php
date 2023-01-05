aee93777373bea2b18193f6e0c1d2807959b9f75
Ivan Sambol <117811964+Sambarone@users.noreply.github.com>
2023-01-05 22:52:00 +0100

    Dorada navbara

--- boot.hr/index.php
+++ boot.hr/index.php
@@ -13,29 +13,17 @@
     
 
     
-    <article class="my-3" id="images">
-      <div class="bd-heading sticky-xl-top align-self-start mt-5 mb-3 mt-xl-0 mb-xl-2">
-        
-      </div>
-
-      
-
-      <div>
-        <div class="bd-example-snippet bd-code-snippet"><div class="bd-example">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg img-fluid" width="100%" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Responsive image" preserveAspectRatio="xMidYMid slice" focusable="false" style="background-image: url('https://cdn.theatlantic.com/thumbor/bKC7yo8z1JGVahVQi7uqAa0CGqk=/0x0:2500x1406/1600x900/media/img/mt/2021/07/AP21172768584842/original.jpg'); background-position-y: center; background-size: 90%;"><title>Placeholder</title><text x="50%" y="50%" fill="#ff2200" dy=".6em">Mjesto gdje počinju snovi</text></svg>
-
-        </div></div>
-
-        
-
-      </div>
-    </article>
+  
 
 
     <article class="my-3" id="navbar">
       <div class="bd-heading sticky-xl-top align-self-start mt-5 mb-3 mt-xl-0 mb-xl-2">
       </div>
+      <div>
+        <div class="bd-example-snippet bd-code-snippet"><div class="bd-example">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg img-fluid" width="100%" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Responsive image" preserveAspectRatio="xMidYMid slice" focusable="false" style="background-image: url('https://cdn.theatlantic.com/thumbor/bKC7yo8z1JGVahVQi7uqAa0CGqk=/0x0:2500x1406/1600x900/media/img/mt/2021/07/AP21172768584842/original.jpg'); background-position-y: center; background-size: 90%;"><title>Placeholder</title><text x="50%" y="50%" fill="#ff2200" dy=".6em">Mjesto gdje počinju snovi</text></svg>
 
+        </div></div>
       <div>
         <div class="bd-example-snippet bd-code-snippet"><div class="bd-example">
         
