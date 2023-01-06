a60b8f0f57afd9dd6920d138552431c30e9182d4
Ivan Sambol <117811964+Sambarone@users.noreply.github.com>
2023-01-06 10:51:21 +0100

    Uređivanje slide-showa

--- boot.hr/slide.php
+++ boot.hr/slide.php
@@ -2,29 +2,63 @@
         <div class="bd-example-snippet bd-code-snippet"><div class="bd-example">
         <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
           <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-label="Slide 1"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2" class=""></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3" class="active" aria-current="true"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3" class=""></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4" class=""></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5" class=""></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5" aria-label="Slide 6" class="" aria-current="true"></button>
           </div>
           <div class="carousel-inner">
+            <div class="carousel-item active">
+              <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: First slide" preserveAspectRatio="xMidYMid slice" focusable="false" style="background-image: url('https://www.tportal.hr/media/thumbnail/w1000/64928.jpeg'); background-position-y: center;"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" style="opacity:0.6"></rect><text x="50%" y="50%" fill="#ffddee" dy=".3em">Giuliano</text></svg>
+
+              <div class="carousel-caption d-none d-md-block">
+                <h5>14. veljače 2023. u 20:00 sati</h5>
+                <p>Športski centar Gripe, Split</p>
+              </div>
+            </div>
+
             <div class="carousel-item">
-              <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: First slide" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#555" dy=".3em">First slide</text></svg>
+              <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: First slide" preserveAspectRatio="xMidYMid slice" focusable="false" style="background-image: url('https://www.kulturauzagrebu.hr/wp-content/uploads/2020/10/bra%C4%8Dna-terapija.jpeg'); background-position-y: center;"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" style="opacity:0.6"></rect><text x="50%" y="50%" fill="#ffddee" dy=".3em">Barčna terapija - Luda kuća</text></svg>
+
+              <div class="carousel-caption d-none d-md-block">
+                <h5>18. veljače 2023. u 19:00 sati</h5>
+                <p>Zagrebačko gradsko kazalište “Komedija”</p>
+              </div>
+            </div>
+
+
+            <div class="carousel-item">
+              <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: First slide" preserveAspectRatio="xMidYMid slice" focusable="false" style="background-image: url('https://www.fia.com/sites/default/files/styles/content_details/public/news/main_image/01121003_392.jpg?itok=7XZetM0X'); background-position-y: center; background-size: 100%;"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" style="opacity:0.5"></rect><text x="50%" y="50%" fill="#555" dy=".3em">First slide</text></svg>
 
               <div class="carousel-caption d-none d-md-block">
                 <h5>First slide label</h5>
                 <p>Some representative placeholder content for the first slide.</p>
               </div>
             </div>
+
             <div class="carousel-item">
-              <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Second slide" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#666"></rect><text x="50%" y="50%" fill="#444" dy=".3em">Second slide</text></svg>
+              <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Second slide" preserveAspectRatio="xMidYMid slice" focusable="false" style="background-image: url('https://m.vecernji.hr/media/img/af/a0/ba9a7b90b114c5436fdb.jpeg'); background-position-y: center; background-size: 100%;"><title>Placeholder</title><rect width="100%" height="100%" fill="#666" style="opacity:0.6"></rect><text x="50%" y="50%" fill="#444" dy=".3em">Second slide</text></svg>
 
               <div class="carousel-caption d-none d-md-block">
                 <h5>Second slide label</h5>
                 <p>Some representative placeholder content for the second slide.</p>
               </div>
             </div>
-            <div class="carousel-item active">
-              <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Third slide" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#555"></rect><text x="50%" y="50%" fill="#333" dy=".3em">Third slide</text></svg>
+
+            <div class="carousel-item">
+              <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Second slide" preserveAspectRatio="xMidYMid slice" focusable="false" style="background-image: url('https://image.dnevnik.hr/media/images/1920x1080/Dec2022/62445329-hrvatska-rukometna-reprezentacija.jpg'); background-position-y: center; background-size: 100%;"><title>Placeholder</title><rect width="100%" height="100%" fill="#666" style="opacity:0.6"></rect><text x="50%" y="50%" fill="#444" dy=".3em">Second slide</text></svg>
+
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+              </div>
+            </div>
+
+
+            <div class="carousel-item">
+              <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Third slide" preserveAspectRatio="xMidYMid slice" focusable="false" style="background-image: url('https://sibenskiportal.hr/wp-content/uploads/2014/08/drazen-zecicx.jpg'); background-position-y: center; background-size: 100%;"><title>Placeholder</title><rect width="100%" height="100%" fill="#555" style="opacity:0.5"></rect><text x="50%" y="50%" fill="#333" dy=".3em">Third slide</text></svg>
 
               <div class="carousel-caption d-none d-md-block">
                 <h5>Third slide label</h5>
