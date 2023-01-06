c9f2cab382f8265118fb69fe2c4edd7cfff815d9
Ivan Sambol <117811964+Sambarone@users.noreply.github.com>
2023-01-06 13:28:25 +0100

    Uređivanje slide-showa

--- boot.hr/slide.php
+++ boot.hr/slide.php
@@ -11,7 +11,7 @@
           </div>
           <div class="carousel-inner">
             <div class="carousel-item active">
-              <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: First slide" preserveAspectRatio="xMidYMid slice" focusable="false" style="background-image: url('https://www.tportal.hr/media/thumbnail/w1000/64928.jpeg'); background-position-y: center;"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" style="opacity:0.6"></rect><text x="50%" y="50%" fill="#ffddee" dy=".3em">Giuliano</text></svg>
+              <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: First slide" preserveAspectRatio="xMidYMid slice" focusable="false" style="background-image: url('https://www.tportal.hr/media/thumbnail/w1000/64928.jpeg'); background-position-y: center%;"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" style="opacity:0.6"></rect><text x="50%" y="50%" fill="#ffddee" dy=".3em">Giuliano</text></svg>
 
               <div class="carousel-caption d-none d-md-block">
                 <h5>14. veljače 2023. u 20:00 sati</h5>
--- zavrsni_rad.hr/slide.php
+++ zavrsni_rad.hr/slide.php
@@ -2,7 +2,7 @@
   <ul class="orbit-container" >
     <button class="orbit-previous" aria-label="previous"><span class="show-for-sr">Previous Slide</span>&#9664;</button>
     <button class="orbit-next" aria-label="next"><span class="show-for-sr">Next Slide</span>&#9654;</button>
-    <li class="is-active orbit-slide" style="background-image: url('../Eros.jpg');" >
+    <li class="is-active orbit-slide" style="background-image: url('https://img.24sata.hr/TskPWpNI09U5oHLAOtCNBjJraMw=/1243x700/smart/media/images/2019-32/pxl_ipa_090219_23520188_yCKaLp2.jpg');" >
       <div class="docs-example-orbit-slide" >
        <pre>
         <h1 style="color:white";>Eros Ramazzotti</h1>
@@ -13,7 +13,7 @@
       </pre>
       </div>
     </li>
-    <li class="orbit-slide" style="background-image: url('../Bebek.jpg')">
+    <li class="orbit-slide" style="background-image: url('https://www.vecernji.hr/media/img/3e/f2/e8538c549144e979db20.jpeg');">
       <div class="docs-example-orbit-slide" >
       <pre>
         <h1 style="color:white";>Željko Bebek</h1>
@@ -24,7 +24,7 @@
       </pre>
       </div>
     </li>
-    <li class="orbit-slide" style="background-image: url('../robie.jpg')">
+    <li class="orbit-slide" style="background-image: url('https://www.nacional.hr/wp-content/uploads/2020/12/93129934_10157455480154107_6146275784588787712_o.jpg');">
       <div class="docs-example-orbit-slide" >
       <pre>
         <h1 style="color:white";>Robie Wiliams</h1>
@@ -35,16 +35,31 @@
       </pre>
       </div>
     </li>
-    <li class="orbit-slide" style="background-image: url('../aco.jpg')">
+
+    <li class="orbit-slide" style="background-image: url('https://www.wfyi.org/files/wfyi/articles/original/lucarossetti-9598-re.jpg');">
       <div class="docs-example-orbit-slide" >
       <pre>
-        <h1 style="color:white; text-align: right;";>Aco Pejović</h1>
-        <h3 style=" color:white; text-align: right;";>11.2.2023. u 20:00 sati</h3>
-        <h4 style="color:white; text-align: right;";>Arena Zagreb</h4>
-       <a class="button float-right" id="ulaznica" >Kupi ulaznice</a>
+        <h1 style="color:white";>Andrea Bocelli</h1>
+        <h3 style="color:white";> 11.2.2023. u 20:00 sati</h3>
+        <h4 style="color:white";>Arena Zagreb</h4>
+       
+        <a class="button float-right" id="ulaznica" >Kupi ulaznice</a>
       </pre>
       </div>
     </li>
+
+    <li class="orbit-slide" style="background-image: url('https://www.lisinski.hr/media/CACHE/images/images/DSC_6398_crop_kVdULN0/906b537bedfa7e21b4aa444877b60d55.jpg');">
+      <div class="docs-example-orbit-slide" >
+      <pre>
+        <h1 style="color:white";>Labuđe jezero - balet</h1>
+        <h3 style="color:white";> 11.2.2023. u 20:00 sati</h3>
+        <h4 style="color:white";>Koncertna dvorana Vatroslava Lisinskog, Zagreb</h4>
+       
+        <a class="button float-right" id="ulaznica" >Kupi ulaznice</a>
+      </pre>
+      </div>
+    </li>
+    
   </ul>
   <nav class="orbit-bullets">
     <button class="is-active" data-slide="0">
@@ -54,5 +69,6 @@
     <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
     <button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>
     <button data-slide="3"><span class="show-for-sr">Fourth slide details.</span></button>
+    <button data-slide="4"><span class="show-for-sr">Fourth slide details.</span></button>
   </nav>
 </div>
