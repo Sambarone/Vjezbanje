91833fbe9a37af2d3f07a8a1c04104b3a929451a
Ivan Sambol <117811964+Sambarone@users.noreply.github.com>
2023-01-08 20:02:12 +0100

    Dodavanje pretraživanja  u nav bar

--- boot.hr/navigacija.php
+++ boot.hr/navigacija.php
@@ -1,37 +1,39 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary mt-5">
-      <div class="container-fluid">
-
-          <div class="btn-group">
-          <button type="button" class="btn btn-primary">Događaji</button>
-          <button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-            <span class="visually-hidden">Toggle Dropdown</span>
-          </button>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Kultura</a></li>
-            <li><a class="dropdown-item" href="#">Glazba</a></li>
-            <li><a class="dropdown-item" href="#">Sport</a></li>
-            <li><a class="dropdown-item" href="#">Obitelj</a></li>
-            <li><a class="dropdown-item" href="#">Ostalo</a></li>
-          </ul>
-        </div><!-- /btn-group -->
-
-        <div class="btn-group">
-          <button type="button" class="btn btn-primary">Lokacije</button>
-          <button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-            <span class="visually-hidden">Toggle Dropdown</span>
-          </button>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Zagreb</a></li>
-            <li><a class="dropdown-item" href="#">Osijek</a></li>
-            <li><a class="dropdown-item" href="#">Split</a></li>
-            <li><a class="dropdown-item" href="#">Rijeka</a></li>
-            <li><a class="dropdown-item" href="#">Zadar</a></li>
-            <li><a class="dropdown-item" href="#">Šibenik</a></li>
-            <li><a class="dropdown-item" href="#">Vukovar</a></li>
-            <li><a class="dropdown-item" href="#">Vinkovci</a></li>
-            <li><a class="dropdown-item" href="#">Varaždin</a></li>
-
-          </ul>
-        </div><!-- /btn-group -->
-      </div>
- </nav>
+          <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent2">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="true">
+                    Lokacije
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Zagreb</a></li>
+                    <li><a class="dropdown-item" href="#">Osijek</a></li>
+                    <li><a class="dropdown-item" href="#">Rijeka</a></li>
+                    <li><a class="dropdown-item" href="#">Split</a></li>
+                    <li><a class="dropdown-item" href="#">Vukovar</a></li>
+                    <li><a class="dropdown-item" href="#">Vinkovci</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Ostale lokacije</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="true">
+                    Događaji
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Kultura</a></li>
+                    <li><a class="dropdown-item" href="#">Glazba</a></li>
+                    <li><a class="dropdown-item" href="#">Sport</a></li>
+                    <li><a class="dropdown-item" href="#">Obitelj</a></li>
+                    <li><a class="dropdown-item" href="#">Ostalo</a></li>
+                    </ul>
+                </li>
+                </ul>
+              <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Pretraži" aria-label="Search">
+                <button class="btn btn-outline-light" type="submit">Pretraži</button>
+              </form>
+            </div>
+          </div>
+        </nav>
