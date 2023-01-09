919c53339fbfcdd72531b6d34f03f4ea4abd8d08
Ivan Sambol <117811964+Sambarone@users.noreply.github.com>
2023-01-09 14:58:55 +0100

    Pješke izrada stranice

---
+++ custom.hr/glava.php
@@ -0,0 +1,11 @@
+<div class="osnovni">
+<h1>Karte.hr</h1>
+<h3>online kupnja ulaznica</h5>
+
+<a href="#0"><button type="button">Prijava</button></a>
+<a href="#0"></a><button type="button">Registracija</button></a>
+<br>
+<br>
+<br>
+
+</div>   
---
+++ custom.hr/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once 'meta.php'?>
+</head>
+<body>
+<?php include_once 'glava.php' ?>
+
+</body>
+</html>
