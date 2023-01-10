ff06494d2a32d87b748cf2ea4dd42b26b81c3c31
Ivan Sambol <117811964+Sambarone@users.noreply.github.com>
2023-01-11 00:40:43 +0100

    Potpuno rješenje zadatka

--- ponavljanjephp.hr/ifvar.php
+++ ponavljanjephp.hr/ifvar.php
@@ -1,20 +1,43 @@
 <?php
 
-$i=isset($_GET['prvi']) ? $_GET['prvi']:0;
-$j=isset($_GET['drugi']) ? $_GET['drugi']:0;
-$k=isset($_GET['treci']) ? $_GET['treci']:0;
+$a=isset($_GET['prvi']) ? $_GET['prvi']:0;
+$b=isset($_GET['drugi']) ? $_GET['drugi']:0;
+$c=isset($_GET['treci']) ? $_GET['treci']:0;
 
 
 
-if (($i===$j)&&($i===$k)){
-    echo 'jednaki su';
+if ($a>$b){
+    if ($a>$c){
+        echo $a;
+    }
+    else if ($a<$c)
+    {
+        echo $c;
+    }
+    else{
+        echo $a,$c;
+    }
 }
-else if (($i>$j)&&($i>$k)){
-    echo 'prvi';
+else if ($b>$a){
+    if ($b>$c){
+        echo $b;
+    }
+    else if ($b<$c)
+    {
+        echo $c;
+    }
+    else{
+        echo $b,$c;
+    }
 }
-else if (($j>$i)&&($j>$k)){
-    echo 'drugi';
-}
-else {
-    echo 'treci';
+else if($a==$b){
+    if ($a==$c){
+        echo $a,$b,$c;
+    }
+    else if ($a<$c){
+        echo $c;
+    }
+    else{
+        echo $a,$b;
+    }
 }
