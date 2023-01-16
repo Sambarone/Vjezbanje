315975714ed7abda1b81eb7100b181d878ff3b68
Ivan Sambol <117811964+Sambarone@users.noreply.github.com>
2023-01-16 10:25:27 +0100

    Uređivanje tablice, dodavanje naslova

--- cilklicnatablica.hr/index.php
+++ cilklicnatablica.hr/index.php
@@ -9,6 +9,14 @@
    
   </head>
   <body class="bg-light">
+
+  <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      
+        <span class="fs-4">Ciklična tablica</span>
+     
+
+      
+    </header>
     
 <div class="container">
   <main>
@@ -17,30 +25,36 @@
     <div class="row g-5">
       
       <div class="col-md-7 col-lg-8">
-        <h4 class="mb-3">Ciklična tablica</h4>
         <form class="needs-validation" novalidate>
           <div class="row g-3">
             <div class="col-sm-6">
               <label for="firstName" class="form-label">Redaka</label>
-              <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
+              <input type="number" class="form-control" id="firstName" placeholder="" value="0" required>
               <div class="invalid-feedback">
-                Valid first name is required.
+                Unesi broj redaka
               </div>
             </div>
 
             <div class="col-sm-6">
               <label for="lastName" class="form-label">Stupaca</label>
-              <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
+              <input type="number" class="form-control" id="lastName" placeholder="" value="0" required>
               <div class="invalid-feedback">
-                Valid last name is required.
+                Unesi broj stupaca
               </div>
             </div>
 
-            
+            <div class="p-4 p-md-5 mb-4 rounded text-bg-dark">
+   
+      
+    <?php include_once 'zadatak5.php'?>
+    
+
+   
+  </div>
 
             
-
-           
+  
+  
 
             
 
@@ -51,14 +65,13 @@
             
           </div>
 
-          <hr class="my-4">
+          
 
          
 
 
           
-
-          
+   
             
 
             
