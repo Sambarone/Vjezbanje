17d5346d40454f50947b96989f3be335054cece1
Ivan Sambol <117811964+Sambarone@users.noreply.github.com>
2023-01-18 09:29:56 +0100

    Pomoću metode post kreiranje ciklicne tablice

--- cilklicnatablica.hr/index.php
+++ cilklicnatablica.hr/index.php
@@ -25,11 +25,11 @@
     <div class="row g-5">
       
       <div class="col-md-7 col-lg-8">
-        <form class="needs-validation" novalidate>
+        <form method="POST" class="needs-validation" novalidate>
           <div class="row g-3">
             <div class="col-sm-6">
               <label for="firstName" class="form-label">Redaka</label>
-              <input type="number" class="form-control" id="firstName" placeholder="" value="5" min="1" max="10" required>
+              <input type="number" class="form-control" name="redak" placeholder="" value="5" min="1" max="10" required>
               <div class="invalid-feedback">
                 Unesi broj redaka
               </div>
@@ -37,14 +37,106 @@
 
             <div class="col-sm-6">
               <label for="lastName" class="form-label">Stupaca</label>
-              <input type="number" class="form-control" id="lastName" placeholder="" value="5" min="1" max="10" required>
+              <input type="number" class="form-control" name="stupac" placeholder="" value="5" min="1" max="10" required>
               <div class="invalid-feedback">
                 Unesi broj stupaca
               </div>
             </div>
 
+            <?php
+$n=isset($_POST['redak'])?$_POST['redak']:5;
+$m=isset($_POST['stupac'])?$_POST['stupac']:5;
+
+
+
+$matrica=[];
+
+if($n>$m){
+$broj_prolaza=$m*2;
+}
+else {
+    $broj_prolaza=($n*2)-1;
+}
+$broj=1;
+
+$redci=$n-1;
+$stupci=$m-1;
+$i=0;
+$j=0;
+$upis=0;
+
+
+for($prolaz=1;$prolaz<=$broj_prolaza;$prolaz++){
+if($prolaz%4===1){
+    $upis=0;;
+    for(;$i<$m;){
+        $matrica[$n-1][$m-1]=$broj;
+        $broj++;
+        $m--;
+        $upis++;
+    }
+    $m=$i;
+    $i++;
+    
+    
+}
+else if($prolaz%4===2){
+    $upis=0;;
+    for(;$j<$n-1;){
+    $matrica[$n-2][$m]=$broj;
+        $broj++;
+        $n--;
+        $upis++;}
+        
+        $n=$j;
+        $j++;
+        
+}
+else if($prolaz%4===3){
+    $upis=0;
+    
+   for($m;$m<$stupci;$m++){
+        $matrica[$n][$m+1]=$broj;
+        $broj++;
+        $upis++;
+    }
+    
+    $stupci--;
+    
+
+}
+else if($prolaz%4===0){
+    $upis=0;
+    $redci--;
+for ($n;$n<$redci;$n++){
+$matrica[$n+1][$m]=$broj;
+        $broj++;
+        $upis++;
+        
+}
+
+$n++;
+
+}
+
+  
+}
+
+$n=isset($_POST['redak'])?$_POST['redak']:5;
+$m=isset($_POST['stupac'])?$_POST['stupac']:5;
+echo '<table>';
+for ($k=0;$k<$n;$k++){
+    echo '<tr>';
+    for ($z=0;$z<$m;$z++) {
+        echo '<td style="text-align:right; padding: 10px;">' . $matrica[$k][$z]. '</td> ';
+         }
+     echo '</tr>';
+     }
+ echo '</table>';
+
+ ?>
             
-            <?php include_once 'zadatak5.php'?>
+            
             
           </div>
           
