1fe7f74b74f441e9d4f1467921a4469c250739d2
Ivan Sambol <117811964+Sambarone@users.noreply.github.com>
2023-01-19 17:11:31 +0100

    Reorganizacija izbornika

--- ponavljanjephp.hr/izbornik.php
+++ ponavljanjephp.hr/izbornik.php
@@ -15,11 +15,7 @@
           <li><a href="helloworld.php">Hello world</a></li>
           <li><a href="kombinacijahtmlphp.php">Kombinacija HTML a i PHP</a></li>
           <li><a href="ulazniparametri.php?kljuc=Ana&X=5">Ulazni parametri</a></li>
-          <li><a target=_blank href="zadatak1.php?boja=red">Zadatak 1</a></li>
-          <li><a target=_blank href="zadatak2.php?grad=Osijek">Zadatak 2</a></li>
-          <li><a target=_blank href="ifvar.php">Zadatak 3</a></li>
-          <li><a target=_blank href="zadatak4.php">Zadatak 4</a></li>
-          <li><a target=_blank href="zadatak5.php?prvi=5&drugi=5">Zadatak 5</a></li>
+          
           <li><a target=_blank href="svemirskibrod.php">Svemirski brod</a></li>
           
         </ul>
@@ -27,9 +23,14 @@
 
       <li class="has-submenu"> <a href="#0">Osnove PHP jezika</a>      
         <ul class="submenu menu vertical" data-submenu>
-          <li><a target=_blank href="varijable.php">Varijable</a></li>
-          <li><a target=_blank href="jednakostvarijable.php">Jednakost Varijable</a></li>
-          <li><a target=_blank href="ugradjenevarijable.php?kljuc=31&t=2">Ugrađene Varijable</a></li>
+          <li> <a href="">Varijable </a> 
+            <ul class="submenu menu vertical" data-submenu>
+              <li><a target=_blank href="varijable.php">Varijable</a></li>
+              <li><a target=_blank href="jednakostvarijable.php">Jednakost Varijable</a></li>
+              <li><a target=_blank href="ugradjenevarijable.php?kljuc=31&t=2">Ugrađene Varijable</a></li>
+            </ul>
+          </li>
+          
           <li><a target=_blank href="operatori.php">Osnovni operatori</a></li>
           <li><a target=_blank href="petljaif.php">If petlja</a></li>
           <li><a target=_blank href="isset.php">Isset</a></li>
@@ -39,11 +40,22 @@
           <li><a target=_blank href="nizovi.php">Nizovi</a></li>
           <li><a target=_blank href="zadatak6.php">Inkrement dekrement</a></li>
           
-
+          
 
         </ul>
     </li>
-    
+   
+    <li class="has-submenu">
+        <a href="#0">Zadaci</a>
+        <ul class="submenu menu vertical" data-submenu>
+          <li><a target=_blank href="zadatak1.php?boja=red">Zadatak 1</a></li>
+          <li><a target=_blank href="zadatak2.php?grad=Osijek">Zadatak 2</a></li>
+          <li><a target=_blank href="ifvar.php">Zadatak 3</a></li>
+          <li><a target=_blank href="zadatak4.php">Zadatak 4</a></li>
+          <li><a target=_blank href="zadatak5.php?prvi=5&drugi=5">Zadatak 5</a></li>
+      
+        </ul>
+    </li>
     </ul>
   </div>
   <div class="top-bar-right">
