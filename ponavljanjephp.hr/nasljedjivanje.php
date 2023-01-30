6fa6af7a701a21864beb3f71a896db67cafdfcff
Ivan Sambol <117811964+Sambarone@users.noreply.github.com>
2023-01-30 22:55:31 +0100

    Nasljeđivanje i apstraktne metode

--- ponavljanjephp.hr/nasljedjivanje.php
+++ ponavljanjephp.hr/nasljedjivanje.php
@@ -2,8 +2,9 @@
 
 include_once 'Pomocno.php';
 
-
-class Osoba{
+//apstraktna klasa je ona koja ne može imati instancu
+//koristi se za nasljeđivanje drugih podklasa
+abstract class Osoba{
     private $ime;
     protected $status;//ovo vide sve klase koje nasljede klasu osoba
 
@@ -22,6 +23,10 @@ class Osoba{
 		$this->ime = $ime;
 	}
 
+    public function __toString(){
+        return $this->ime;
+    }
+
     
 }
 
@@ -29,8 +34,8 @@ class Polaznik extends Osoba
 {
         private $brUgovora;
 
-        public function __construct($ime='', $brUgovora){
-            parrent::__constrct($ime);
+        public function __construct($ime='', $brUgovora=''){
+            parent::__construct($ime);
             // ime se može i ovako postaviti
             //$this->ime=$ime;
             $this->getBrUgovora=$brUgovora;
@@ -46,6 +51,10 @@ class Polaznik extends Osoba
         public function setBrUgovora($brUgovora){
             $this->brUgovora = $brUgovora;
         }
+                //override
+        public function setIme($ime){
+            parent::setIme ( $ime . 'Pregaženo');
+        }
 
 
 
@@ -56,7 +65,7 @@ class Predavac extends Osoba
         private $iban;
 
         public function __construct($ime='', $iban){
-            parrent::__constrct($ime);
+            parent::__construct($ime);
             // ime se može i ovako postaviti
             //$this->ime=$ime;
             $this->getBrUgovora=$iban;
@@ -72,8 +81,29 @@ class Predavac extends Osoba
         $this->iban = $iban;
     }
 
+    public function __toString(){
+        return $this->status. ': '. $this->getIme();
+    }
+
 
 
 }
 
+class Ravnatelj extends Osoba{
 
+    
+}
+
+$polaznik=new Polaznik('Pero', '12/2023');
+$polaznik->setIme('Karlo');
+
+Pomocno::log($polaznik->getIme());
+
+$predavac=new Predavac('Marija', '12/2023');
+$predavac->setIme('Zrinka');
+
+Pomocno::log($predavac->getIme());
+
+echo $predavac;
+echo '<hr>';
+echo $polaznik;
